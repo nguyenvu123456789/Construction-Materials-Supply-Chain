a90e3a9bd163766ce50464bf7746c444eb95318c
v1perii2572 <v1perii2570202@gmail.com>
2025-09-30 16:46:34 +0700

    fix materialDAO

--- Construction_Materials_Supply_Chain/DataAccess/MaterialDAO.cs
+++ Construction_Materials_Supply_Chain/DataAccess/MaterialDAO.cs
@@ -1,47 +1,47 @@
 ﻿using BusinessObjects;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DataAccess
 {
-    public class MaterialDAO
+    public class MaterialDAO : BaseDAO
     {
-        private readonly ScmVlxdContext _context;
-
-        public MaterialDAO(ScmVlxdContext context)
-        {
-            _context = context;
-        }
+        public MaterialDAO(ScmVlxdContext context) : base(context) { }
 
         public List<Material> GetMaterials()
         {
-            return _context.Materials.Include(m => m.Category).ToList();
+            return Context.Materials
+                           .Include(m => m.Category)
+                           .ToList();
         }
 
         public Material? GetMaterialById(int id)
         {
-            return _context.Materials.Include(m => m.Category)
-                                     .FirstOrDefault(m => m.MaterialId == id);
+            return Context.Materials
+                           .Include(m => m.Category)
+                           .FirstOrDefault(m => m.MaterialId == id);
         }
 
         public void AddMaterial(Material material)
         {
-            _context.Materials.Add(material);
-            _context.SaveChanges();
+            Context.Materials.Add(material);
+            Context.SaveChanges();
         }
 
         public void UpdateMaterial(Material material)
         {
-            _context.Materials.Update(material);
-            _context.SaveChanges();
+            Context.Materials.Update(material);
+            Context.SaveChanges();
         }
 
         public void DeleteMaterial(int id)
         {
-            var material = _context.Materials.Find(id);
+            var material = Context.Materials.Find(id);
             if (material != null)
             {
-                _context.Materials.Remove(material);
-                _context.SaveChanges();
+                Context.Materials.Remove(material);
+                Context.SaveChanges();
             }
         }
     }

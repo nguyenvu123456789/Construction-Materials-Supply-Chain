b56fa8d42feaba88f4f9cfe22f93db6546de591a
v1perii2572 <v1perii2570202@gmail.com>
2025-10-06 16:55:23 +0700

    add fullname to dto

--- Construction_Materials_Supply_Chain/Application/DTOs/UserDto.cs
+++ Construction_Materials_Supply_Chain/Application/DTOs/UserDto.cs
@@ -4,6 +4,7 @@
     {
         public int UserId { get; set; }
         public string UserName { get; set; } = string.Empty;
+        public string FullName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string Phone { get; set; } = string.Empty;
         public string AvatarUrl { get; set; } = string.Empty;

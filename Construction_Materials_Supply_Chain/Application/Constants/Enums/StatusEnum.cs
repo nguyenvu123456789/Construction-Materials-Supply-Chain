8efb111e0c616d79ffa0def5fdcde2bcfcf67acb
LAPTOP-9C27KAAK\Admin <nguyenvu150203@gmail.com>
2025-11-30 12:25:30 +0700

    Refactor Export

---
+++ Construction_Materials_Supply_Chain/Application/Constants/Enums/StatusEnum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Application.Constants.Enums
+{
+    public enum StatusEnum
+    {
+        Pending = 1,        // Chờ xử lý
+        Approved = 2,       // Đã duyệt
+        Rejected = 3,       // Từ chối
+        InProgress = 4,     // Đang xử lý
+        Completed = 5,      // Hoàn thành
+        Canceled = 6,       // Hủy
+        Draft = 7,          // Nháp
+        Success = 8         // Thành công
+    }
+
+    public static class StatusEnumExtensions
+    {
+        public static string ToStatusString(this StatusEnum status)
+        {
+            return status.ToString();
+        }
+    }
+}
